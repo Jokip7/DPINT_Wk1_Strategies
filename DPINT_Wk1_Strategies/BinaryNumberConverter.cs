62cb5e360f7b2a9af869b735c0dbf39ba8fb02b8
Chris Meesters <Jokip7>
2016-11-07 13:18:11 +0100

    Structuring

---
+++ DPINT_Wk1_Strategies/BinaryNumberConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPINT_Wk1_Strategies
+{
+    class BinaryNumberConverter : INumberConverter
+    {
+        public string ToLocalString(int number)
+        {
+            return Convert.ToString(number, 2);
+        }
+
+        public int ToNumerical(string value)
+        {
+            return Convert.ToInt32(value, 2);
+        }
+    }
+}
---
+++ DPINT_Wk1_Strategies/ConverterFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPINT_Wk1_Strategies
+{
+    class ConverterFactory
+    {
+        public IEnumerable<String> ConverterNames
+        {
+            get { return _converters.Keys; }
+        }
+
+        public Dictionary<string, INumberConverter> _converters;
+
+        public ConverterFactory()
+        {
+            _converters = new Dictionary<string, INumberConverter>();
+        }
+    }
+}
